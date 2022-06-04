611eb050a83560eb9369bfd2393331bf896b9461
Alireza Safaierad <frontendmonster@gmail.com>
2022-06-04 18:27:09 +0400

    add base canvas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
+import style from './App.module.css'
+
 const App = () => {
-  return <div className="App">Hello</div>;
-}
+  return <div className={style.canvas} id="canvas" contentEditable></div>;
+};
 
 export default App;
