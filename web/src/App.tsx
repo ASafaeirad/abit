8eb549de0ce767e37bdddad2b39d5729cc99f545
Alireza Safaierad <frontendmonster@gmail.com>
2022-06-04 21:14:52 +0400

    make editable blocks selectable

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,32 @@
-import style from './App.module.css'
+import { useState } from 'react';
+
+import style from './App.module.css';
 
 const App = () => {
-  return <div className={style.canvas} id="canvas" contentEditable></div>;
+  const [selected, setSelected] = useState(0);
+
+  return (
+    <div className={style.app}>
+      <div
+        role="button"
+        tabIndex={0}
+        onKeyDown={() => setSelected(0)}
+        className={style.block}
+        onClick={() => setSelected(0)}
+        id="canvas"
+        contentEditable={selected === 0}
+      />
+      <div
+        role="button"
+        tabIndex={0}
+        onKeyDown={() => setSelected(1)}
+        className={style.block}
+        onClick={() => setSelected(1)}
+        id="canvas"
+        contentEditable={selected === 1}
+      />
+    </div>
+  );
 };
 
 export default App;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,8 @@
-import './global.css'
+import './global.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 
 const root = document.getElementById('root')!;
